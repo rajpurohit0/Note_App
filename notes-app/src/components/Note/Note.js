10c5f989d4490c6a8609dae5c79d308feffc47db
rajpurohit0 <rajpurohit0298@gmail.com>
2023-07-08 12:45:18 +0530

    program

--- notes-app/src/components/Note/Note.js
+++ notes-app/src/components/Note/Note.js
@@ -1,23 +1,35 @@
 import React from 'react';
 
+import deleteIcon from '../../assets/delete.png';
+
 import './Note.css';
 
-let timer=500, timeout
-
-import deleteIcon from '../../assets/delete.png'
+let timer=500, 
+  timeout;
 
 export default function Note(props) {
 
   const formatDate=(value)=>{
     if(!value) return "";
 
-    const date=new date(value)
-    const monthNames=['Jan','Feb','March','April','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-
+    const date=new Date(value);
+    const monthNames=[
+      "Jan",
+      "Feb",
+      "March",
+      "April",
+      "May",
+      "Jun",
+      "Jul",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec"
+    ];
     let hrs=date.getHours()
-    let amPm=hrs>12? "PM" : "AM";
-    hrs=hrs? hrs : "12";
-    hrs=hrs>12? (hrs=24-hrs) : hrs;
+    let amPm=hrs>=12? "PM" : "AM";
+    hrs=hrs ? hrs : "12";
+    hrs=hrs>12 ? (hrs=24-hrs) : hrs;
     
     let min=date.getMinutes();
     min=min<10? "0" + min : min;
@@ -27,6 +39,7 @@ export default function Note(props) {
 
     return `${hrs}:${min} ${amPm} ${day} ${month}`;
   };
+  console.log(formatDate);
 
   const debounce=(func)=>{
     clearTimeout(timeout)
@@ -38,11 +51,11 @@ export default function Note(props) {
   };
 
   return (
-    <div className='note' style={{backgroundColor:props.note.color}}>
+    <div className='note' style={{backgroundColor: props.note.color}}>
       <textarea 
         className='note_text' 
         defaultValue={props.note.text}
-         onChange={(event)=>updateText(event.target.value,props.note.id)}
+         onChange={(event)=>updateText(event.target.value, props.note.id)}
          /> 
       <div className='note_footer'>
         <p>{formatDate(props.note.time)}</p>
